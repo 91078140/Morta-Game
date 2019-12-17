e4a63e0b1f7d8840cd3f2f03b47a90ce0df92380
Timothy Wilks <91078140@mvusd.net>
2019-12-17 12:14:55 -0800

    The src of my game

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -16,13 +16,13 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor
 	Texture morta, morter, grid, sand, white;
 	private int[] drop = new int[5];
 	private int[] dropy = new int[5];
-	private int x1 = 165, y1 = 100, xm = 173, ym = 600, ym1;
+	private int x1 = 0, y1 = 0, xm = 0, ym = 600, ym1, xm1;
 	private double count = 1;
 	private Rectangle bombBounds, mortaBounds;
 
 
 
-
+// creates textures and sets the missiles parameters
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -34,14 +34,17 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor
 
 		for(int i = 0; i < drop.length; i++)
 		{
-			drop[i] = 173 + (72 * i);
+			xm1 = 0;
+			while(xm1 < 100)
+			{
+				xm1 += (int) (Math.random() * 100);
+			}
+			drop[i] = (xm1) * i - 100;
 		}
 		for(int i = 0; i < dropy.length; i++)
 		{
 			dropy[i] = ym;
 		}
-
-
 	}
 
 	@Override
@@ -54,8 +57,8 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor
 				drop[3] >= x1 - 25 && drop[3] <= x1 + 25 && dropy[3] >= y1 - 25 && dropy[3] <= y1 + 25 ||
 				drop[4] >= x1 - 25 && drop[4] <= x1 + 25 && dropy[4] >= y1 - 25 && dropy[4] <= y1 + 25)
 		{
-				x1 = 165;
-				y1 = 100;
+				x1 = 0;
+				y1 = 0;
 				xm = 173;
 			for(int i = 0; i < dropy.length; i++)
 			{
@@ -74,23 +77,40 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor
 			if(dropy[0] < 0)
 			{
 				ym1 = 0;
-					while(ym1 < 500)
-					{
-						ym1 += (int) (Math.random() * 100);
-					}
-					dropy[0] = ym + ym1;
+				xm1 = 0;
+				while(ym1 < 500)
+				{
+					ym1 += (int) (Math.random() * 100);
+				}
+				dropy[0] = ym + ym1;
+
+				while(xm1 < 100)
+				{
+					xm1 += (int) (Math.random() * 100);
+				}
+				drop[0] = xm1;
+
 				count += .05;
 			}// end of if that resets the bombs to the top
-		if(dropy[1] < 0)
-		{
+
+			if(dropy[1] < 0)
+			{
 			ym1 = 0;
+			xm1 = 0;
 			while(ym1 < 500)
 			{
 				ym1 += (int) (Math.random() * 100);
 			}
 			dropy[1] = ym + ym1;
+
+			while(xm1 < 100)
+			{
+				xm1 += (int) (Math.random() * 100);
+			}
+			drop[1] = xm1 - 100;
 			count += .05;
 		}// end of if that resets the bombs to the top
+
 		if(dropy[2] < 0)
 		{
 			ym1 = 0;
@@ -99,8 +119,15 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor
 				ym1 += (int) (Math.random() * 100);
 			}
 			dropy[2] = ym + ym1;
+			xm1 = 0;
+			while(xm1 < 100)
+			{
+				xm1 += (int) (Math.random() * 100);
+			}
+			drop[2] = (xm1) * 2 - 100;
 			count += .05;
 		}// end of if that resets the bombs to the top
+
 		if(dropy[3] < 0)
 		{
 			ym1 = 0;
@@ -109,8 +136,15 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor
 				ym1 += (int) (Math.random() * 100);
 			}
 			dropy[3] = ym + ym1;
+			xm1 = 0;
+			while(xm1 < 100)
+			{
+				xm1 += (int) (Math.random() * 100);
+			}
+			drop[3] = (xm1) * 3 - 100;
 			count += .05;
 		}// end of if that resets the bombs to the top
+
 		if(dropy[4] < 0)
 		{
 			ym1 = 0;
@@ -119,37 +153,45 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor
 				ym1 += (int) (Math.random() * 100);
 			}
 			dropy[4] = ym + ym1;
+			xm1 = 0;
+			while(xm1 < 100)
+			{
+				xm1 += (int) (Math.random() * 100);
+			}
+			drop[4] = (xm1) * 4 - 100;
 			count += .05;
 		}// end of if that resets the bombs to the top
+
 		// detects the arrow keys and moves the character
 			if (Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)) {
-				if (x1 > 165) {
+				if (x1 > 0) {
 					x1 -= 5;
 				}
 			}
 			if (Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)) {
-				if (x1 < 455) {
+				if (x1 < 580) {
 					x1 += 5;
 				}
 			}
 			if (Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN)) {
-				if (y1 > 100) {
+				if (y1 > 0) {
 					y1 -= 5;
 				}
 			}
 			if (Gdx.input.isKeyPressed(Input.Keys.DPAD_UP)) {
-				if (y1 < 310) {
+				if (y1 < 420) {
 					y1 += 5;
 				}
 			}
+
 		    bombBounds = new Rectangle(xm, ym, 60, 60);
 		    mortaBounds = new Rectangle(x1, y1, 60, 60);
-			Gdx.gl.glClearColor(1, 0, 0, 1);
+			Gdx.gl.glClearColor(1, 1, 1, 1);
 			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 			batch.begin();
-			batch.draw(sand, 0, 0, 800, 600);
-			batch.draw(grid, 20, 45, 600, 400);
-			batch.draw(white, 240, 180, 60, 60);
+			//batch.draw(sand, 0, 0, 800, 600);
+			//batch.draw(grid, 20, 45, 600, 400);
+			//batch.draw(white, 0, 0, 800, 600);
 			batch.draw(morta, x1, y1, 60, 60);
 			batch.draw(morter, drop[0], dropy[0], 60, 60);// 1
 			batch.draw(morter, drop[1], dropy[1], 60, 60);// 2
